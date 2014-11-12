985c47de9b7e8c7276bbe2ada7693cbae0e0136d
lala-mac <mwj7999@hotmail.com>
2014-11-13 01:13:48 +0800

    去掉拦截

--- mobile-cms-web/src/main/java/com/joinsoft/mobile/cms/web/CmsUIInterceptors.java
+++ mobile-cms-web/src/main/java/com/joinsoft/mobile/cms/web/CmsUIInterceptors.java
@@ -7,20 +7,13 @@ import com.joinsoft.framework.web.UIInterceptors;
 import com.joinsoft.mobile.cms.component.Http;
 import com.joinsoft.mobile.cms.service.mobile.MobileSecurityService;
 import com.joinsoft.mobile.cms.service.mobile.MobileService;
-import com.joinsoft.mobile.cms.web.front.security.AccessTokenController;
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.UsernamePasswordToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.net.URLEncoder;
 
 /**
  * xingsen@join-cn.com
@@ -42,71 +35,71 @@ public class CmsUIInterceptors extends UIInterceptors {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        String openid = request.getParameter("openId");
-        String time = request.getParameter("time");
-        if (!StringUtils.isEmpty(openid) && !StringUtils.isEmpty(time)) {
-            String signature = new String(DigestUtils.md5Hex(String.format("%s@%s", openid, time)));
-            if (signature.equals(request.getParameter("signature"))) {
-                //查找用户是否存在，如果不存在，增加用户
-                User user = userRepository.findByLoginName(openid);
-                if (null == user) {
-                    securityService.registerUser(openid);
-                }
-                UsernamePasswordToken token = new UsernamePasswordToken(openid, openid);
-                SecurityUtils.getSubject().login(token);
-            }
-
-        }
-        if (handler instanceof HandlerMethod &&
-                (((HandlerMethod) handler).getBean() instanceof AccessTokenController)) {
-            if (securityService.isAdmin()) {
-                return true;
-            }
-            if (request.getRequestURI().indexOf("/auth") != -1) {
-                return true;
-            }
-            if (!securityService.isBind()) {
-                response.sendRedirect(request.getContextPath() + "/front/at/auth/login.do");
-                return false;
-            }
-
-            if (securityService.getLoginUser() != null) {
-                //用户已经登录则执行正常逻辑
-                return true;
-            }
-
-            if (request.getMethod().equalsIgnoreCase("post") && !isLogin(request)) {
-                logger.warn("用户请求过期");
-                request.getRequestDispatcher("/jsp/500.jsp").forward(request, response);
-                return false;
-            }
-
-            if (request.getMethod().equalsIgnoreCase("get")) {
-                String code = request.getParameter("code");
-                //如果用户没有code则跳转
-                if (StringUtils.isEmpty(code)) {
-                    //用户没有拿到code则跳转到微信
-                    String reqURI = URLEncoder.encode(getFullUrl(request), "UTF-8");
-
-                    String url = mobileService.buildGetCodeUrl(reqURI);
-                    logger.info("redirect url {}", url);
-                    response.sendRedirect(url);
-                    return false;
-                }
-                if (isLogin(request)) {
-                    //登录界面不做自动登录,防止死循环
-                    return true;
-                }
-                //根据openid登录
-                String openId = mobileService.getOpenId(code);
-                boolean isLogin = securityService.ifBindThenLogin(openId);
-                if (!isLogin) {
-                    //登录失败则认为用户还未绑定
-                    response.sendRedirect(request.getContextPath() + "/front/at/auth/login.do");
-                    return false;
-                }
-            }
-        }
+//        String openid = request.getParameter("openId");
+//        String time = request.getParameter("time");
+//        if (!StringUtils.isEmpty(openid) && !StringUtils.isEmpty(time)) {
+//            String signature = new String(DigestUtils.md5Hex(String.format("%s@%s", openid, time)));
+//            if (signature.equals(request.getParameter("signature"))) {
+//                //查找用户是否存在，如果不存在，增加用户
+//                User user = userRepository.findByLoginName(openid);
+//                if (null == user) {
+//                    securityService.registerUser(openid);
+//                }
+//                UsernamePasswordToken token = new UsernamePasswordToken(openid, openid);
+//                SecurityUtils.getSubject().login(token);
+//            }
+//
+//        }
+//        if (handler instanceof HandlerMethod &&
+//                (((HandlerMethod) handler).getBean() instanceof AccessTokenController)) {
+//            if (securityService.isAdmin()) {
+//                return true;
+//            }
+//            if (request.getRequestURI().indexOf("/auth") != -1) {
+//                return true;
+//            }
+////            if (!securityService.isBind()) {
+////                response.sendRedirect(request.getContextPath() + "/front/at/auth/login.do");
+////                return false;
+////            }
+//
+//            if (securityService.getLoginUser() != null) {
+//                //用户已经登录则执行正常逻辑
+//                return true;
+//            }
+//
+//            if (request.getMethod().equalsIgnoreCase("post") && !isLogin(request)) {
+//                logger.warn("用户请求过期");
+//                request.getRequestDispatcher("/jsp/500.jsp").forward(request, response);
+//                return false;
+//            }
+//
+//            if (request.getMethod().equalsIgnoreCase("get")) {
+//                String code = request.getParameter("code");
+//                //如果用户没有code则跳转
+//                if (StringUtils.isEmpty(code)) {
+//                    //用户没有拿到code则跳转到微信
+//                    String reqURI = URLEncoder.encode(getFullUrl(request), "UTF-8");
+//
+//                    String url = mobileService.buildGetCodeUrl(reqURI);
+//                    logger.info("redirect url {}", url);
+//                    response.sendRedirect(url);
+//                    return false;
+//                }
+//                if (isLogin(request)) {
+//                    //登录界面不做自动登录,防止死循环
+//                    return true;
+//                }
+//                //根据openid登录
+//                String openId = mobileService.getOpenId(code);
+//                boolean isLogin = securityService.ifBindThenLogin(openId);
+//                if (!isLogin) {
+//                    //登录失败则认为用户还未绑定
+//                    response.sendRedirect(request.getContextPath() + "/front/at/auth/login.do");
+//                    return false;
+//                }
+//            }
+//        }
         return true;
     }
 
