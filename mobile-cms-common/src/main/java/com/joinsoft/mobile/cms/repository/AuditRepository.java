55d23d725dde3e3dbfa57b51904345d724f3d4f7
chenmingde <chenmingde@shtel.com.cn>
2015-01-02 14:39:36 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	mobile-cms-web/src/main/resources/config/core/config.properties

--- mobile-cms-common/src/main/java/com/joinsoft/mobile/cms/repository/AuditRepository.java
+++ mobile-cms-common/src/main/java/com/joinsoft/mobile/cms/repository/AuditRepository.java
@@ -1,16 +1,14 @@
 package com.joinsoft.mobile.cms.repository;
 
 import com.joinsoft.framework.orm.jpa.JpaRepository;
+import com.joinsoft.mobile.cms.entity.TbAudit;
 import com.joinsoft.mobile.cms.entity.agent.TbAgent;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 
-public interface AuditRepository extends JpaRepository<TbAgent, Long> {
-    @Query("FROM TbAgent WHERE agentName=?1 ")
-    TbAgent findByActionName(String agentName);
+public interface AuditRepository extends JpaRepository<TbAudit, Long> {
 
-    @Query("from TbAgent t where t.section.id=?1")
-    List<TbAgent> findByTbSectionAgentId(Long id);
+
 }
